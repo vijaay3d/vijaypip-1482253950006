959c7ce9a32fad0dd1f9ea534b7f94acbe21c65d
Vijay Prabhakar <vijaay3d@gmail.com>
2016-12-20 15:31:34 -0600

    Added print

--- hello.py
+++ hello.py
@@ -23,12 +23,28 @@ def hello_world():
     watsonUrl = 'https://twcservice.mybluemix.net/api/weather/v1/location/' + zip + ':4:US' + '/observations.json?language=en-US'
     try:
         r = requests.get(watsonUrl,auth=(username,password))
-        return r.text
+        #print json.dumps(parsed, indent=4, sort_keys=True) 
+        results = r.text
+        if results != False:
+            results = json.loads(str(results))
+            display_weather(results)
+        else:
+            print('Something went wrong :-(')
     except:
         return False
 
 #def hello_world():
 #    return 'Hello World! I am running on port ' + str(port)
+
+def display_weather(results):
+    print()
+    print('Here is the weather for {0}'.format(results['observation']['obs_name']))
+    print('{0:20} {1:<10}'.format('Current Temperature:',str(results['observation']['temp']) + '° and ' + results['observation']['wx_phrase']))
+    print('{0:20} {1:<10}'.format('Feels Like: ',str(results['observation']['feels_like']) + '°'))
+    print('{0:20} {1:<10}'.format('Low Temp: ',str(results['observation']['min_temp']) + '°'))
+    print('{0:20} {1:<10}'.format('High Temp: ',str(results['observation']['max_temp']) + '°'))
+    print('{0:20} {1:<10}'.format('Winds:',str(results['observation']['wspd']) + ' mph coming from the ' + results['observation']['wdir_cardinal']))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
